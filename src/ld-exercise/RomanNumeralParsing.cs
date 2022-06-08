b9e69a2c649b9698152ebcca8004172a883f5f11
izsi-salmon <izsi.salmon@gmail.com>
2022-06-08 17:51:45 +1200

    refactored and cleaned up

--- src/ld-exercise/RomanNumeralParsing.cs
+++ src/ld-exercise/RomanNumeralParsing.cs
@@ -9,15 +9,15 @@ namespace ld_exercise
         // A dictionary, inside <> is <key, value>
 
         // Telling it that it is read only lets the compiler know that it doesn't need to onclude the code required to change it, and it will warn us if we try and change it
-        private readonly Dictionary<int, string> _romanNumerals = new Dictionary<int, string>()
+        private readonly Dictionary<int, char> _romanNumerals = new Dictionary<int, char>()
         {
-            { 1000, "M" },
-            { 500, "D" },
-            { 100, "C" },
-            { 50, "L" },
-            { 10, "X" },
-            { 5, "V" },
-            { 1, "I" }
+            { 1000, 'M' },
+            { 500, 'D' },
+            { 100, 'C' },
+            { 50, 'L' },
+            { 10, 'X' },
+            { 5, 'V' },
+            { 1, 'I' }
         };
 
         public void PrintRomanNumerals(int input)
@@ -27,87 +27,27 @@ namespace ld_exercise
             Console.WriteLine(result);
         }
 
-        // Can test this method using the unit tests
         public string ParseRomanNumerals(int input)
         {
-            // Pseudo code:
-            // For inputs under 10
-            // Take the input and modulo it by 5
-            // Add a V for each 5, add a I for each remainder
-
-            string output = "";
+            StringBuilder result = new StringBuilder();
 
             int total = input;
 
-            int numberOfThousands = input / 1000;
-
-            // string result = "";
-
-            StringBuilder result = new StringBuilder();
-
-            for (int i = 0; i < numberOfThousands; i++)
+            void Processor(int romanNumeralNumber, char romanNumeralSymbol)
             {
-                result.Append(_romanNumerals[1000]);
-                total -= 1000;
+                string numberOfNumerals = new string(romanNumeralSymbol, total / romanNumeralNumber);
+
+                result.Append(numberOfNumerals);
+
+                total %= romanNumeralNumber;
             }
 
-            int numberOfFiveHundreds = total / 500;
-
-            for (int i = 0; i < numberOfFiveHundreds; i++)
+            foreach (var romanNumeral in _romanNumerals)
             {
-                result.Append(_romanNumerals[500]);
-                total -= 500;
+                Processor(romanNumeral.Key, romanNumeral.Value);
             }
 
-            int numberOfHundreds = total / 100;
-
-            for (int i = 0; i < numberOfHundreds; i++)
-            {
-                result.Append(_romanNumerals[100]);
-                total -= 100;
-            }
-
-            int numberOfFifties = total / 50;
-
-            for (int i = 0; i < numberOfFifties; i++)
-            {
-                result.Append(_romanNumerals[50]);
-                total -= 50;
-            }
-
-            int numberOfTens = total / 10;
-
-            for (int i = 0; i < numberOfTens; i++)
-            {
-                result.Append(_romanNumerals[10]);
-                total -= 10;
-            }
-
-
-            int numberOfFives = total / 5;
-
-            for (int i = 0; i < numberOfFives; i++)
-            {
-                result.Append(_romanNumerals[5]);
-                total -= 5;
-            }
-
-            int numberOfOnes = total;
-
-            for (int i = 0; i < numberOfOnes; i++)
-            {
-                result.Append(_romanNumerals[1]);
-            }
-
-            // foreach (var item in _romanNumerals)
-            // {
-            //     //     int numerators = input / item.Key;
-            //     //     input %= item.Key;
-            // }
-
-            output = result.ToString();
-
-            return output;
+            return result.ToString();
         }
     }
 }
